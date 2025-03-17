6d6c3de0fab667acec869f5314ade789ec606ee4
EmilieTr <emilie@terhaar.info>
2025-03-17 16:50:41 +0100

    question 6 visualization

---
+++ Visualizations/question_6/question6.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import plotly.graph_objects as go
+
+
+def ranking_age():
+    def format_name(name):
+        # only change format if name contains comma
+        if "," in name:
+            surname, first_name = name.split(", ", 1)  # divide name
+            surname = surname.capitalize()  # only first letter is capitalized
+            first_name = first_name.strip() 
+            return f"{first_name} {surname}" #combine name again
+        
+        return name.strip()
+
+    list_player = []
+    for year in range(2009, 2025):
+        file = f'Data/order_of_merit/order_of_merit_year_{year}.csv'
+        df = pd.read_csv(file)
+        name = df[(df['Aktuelle Position'] == 1)]['Name'].iloc[0]
+        list_player.append(format_name(name))
+    
+    file_players = 'Data/question 6/male_players.csv'
+    df_players = pd.read_csv(file_players)
+
+    nationalities = ['England', 'Portugal', 'Deutschland', 'Schottland', 'Niederlande', 'Irland', 'Schweiz', 'Anderes']
+    order_of_merit = []
+    counter = []
+    for i in range(2):
+        total = 0
+        for country in nationalities[:len(nationalities)-1]:
+            counter_value = df_players['Nationality'].value_counts().get(country, 0)
+            counter.append(counter_value)
+            total += counter_value
+            order_of_merit.append(i+1)
+        counter.append(len(df_players)- total)
+        order_of_merit.append(i+1)
+    
+    fig = go.Figure()
+
+    
+    fig.add_trace(go.Scatter(
+    x=nationalities,
+    y=order_of_merit,
+    mode="markers",  # Nur Punkte (keine Linien)
+    marker=dict(
+        size=[s / 2 for s in counter],  # Blasengröße (angepasst für bessere Skalierung)
+        color=order_of_merit,  # Farbverlauf basierend auf der Quote
+        colorscale="Plasma",  # Farbschema
+        showscale=True  # Farblegende anzeigen
+    ),
+    text=[f"Spiele: {s}" for s in counter],  # Hover-Text
+    ))
+
+    # Layout anpassen
+    fig.update_layout(
+        title="Blasendiagramm: Checkout-Quote über die Jahre",
+        xaxis_title="Jahr",
+        yaxis_title="Checkout-Quote (%)",
+        yaxis_tickformat=".0%",  # Prozentwerte auf Y-Achse
+        template="plotly_white"
+    )
+
+    return fig
+
+fig = ranking_age()
+fig.show()
