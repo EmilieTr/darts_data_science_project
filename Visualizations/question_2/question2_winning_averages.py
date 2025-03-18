b2ba14b030d872b0138e394ff5339a55f0544774
Sara <Sara@example.com>
2025-03-18 22:29:41 +0100

    solved some tasks

--- Visualizations/question_2/question2_winning_averages.py
+++ Visualizations/question_2/question2_winning_averages.py
@@ -1,26 +1,34 @@
 import pandas as pd
 import plotly.graph_objects as go
 import re
+import streamlit as st
 
-def plot_winning_averages(tournament):
-    
+def plot_winning_averages(selected_tournaments):
+    #st.write(selected_tournaments)
     # List of all major tournaments
-    major_tournaments = [
-        "All",
+    major_tournaments_all = [
         "World Championship",
         "World Matchplay",
         "World Grand Prix",
-        "Masters of Darts",
-        "PDC US Open", 
         "Grand Slam",
         "Players Championship Finals",
         "World Cup",
-        "World Masters",
         "World Series of Darts Finals",
-        "Champions League"
     ]
-    extra_tournaments = ("European Tour", "Players Championship", "World Series")
+    extra_tournaments_all = ["European Tour", "Players Championship", "World Series"]
+    
+    major_tournaments = []
+    extra_tournaments = []
+    only_one = False
 
+    if len(selected_tournaments) == 1:
+        only_one = True
+    for tournament in selected_tournaments:
+        if tournament in major_tournaments_all:
+            major_tournaments.append(tournament)
+        elif tournament in extra_tournaments_all:
+            extra_tournaments.append(tournament)
+    #st.write(major_tournaments)
     # Load CSV file
     file = 'Data/question 2/question2.csv'
     df = pd.read_csv(file)
@@ -31,35 +39,41 @@ def plot_winning_averages(tournament):
     # Filter data without average and average < 10
     df_cleaned = df.dropna(subset=['Average'])
     df_cleaned = df_cleaned[df_cleaned['Average'] >= 10]
-
+    
     # Filter data of majors and extra tournaments
     df_all = df_cleaned[
-        df_cleaned['Tournament'].isin(major_tournaments) | 
-        df_cleaned['Tournament'].str.startswith(extra_tournaments, na=False) & 
+        df_cleaned['Tournament'].isin(major_tournaments_all) | 
+        df_cleaned['Tournament'].str.startswith(tuple(extra_tournaments_all), na=False) & 
         ~df_cleaned['Tournament'].str.contains("Qualifier", na=False)
     ]
 
-    if tournament == "All":
+    if (major_tournaments + extra_tournaments) == (major_tournaments_all + extra_tournaments_all):
         df_selected = df_all
-    elif tournament in extra_tournaments:
+    elif only_one == True:
         # Filter data of majors and extra tournaments
         df_selected = df_cleaned[
-            df_cleaned['Tournament'].str.startswith(tournament, na=False) & 
+            (df_cleaned['Tournament'] == selected_tournaments[0]) 
+        ]
+    elif extra_tournaments != "":
+        # Filter data of majors and extra tournaments
+        df_selected = df_cleaned[
+            df_cleaned['Tournament'].isin(major_tournaments) |
+            df_cleaned['Tournament'].str.startswith(tuple(extra_tournaments), na=False) & 
             ~df_cleaned['Tournament'].str.contains("Qualifier", na=False)
         ]
     else:
-        # Filter data of majors and extra tournaments
         df_selected = df_cleaned[
-            (df_cleaned['Tournament'] == tournament) 
-        ]
-
+            df_cleaned['Tournament'].isin(major_tournaments) | 
+            ~df_cleaned['Tournament'].str.contains("Qualifier", na=False)
+        ]  
+    #st.write(df_selected.head())
     # Normalize tournament names (remove numbers)
     def normalize_tournament_name(name):
         return re.sub(r'\d+', '', name).strip()  # Removes numbers and extra spaces
 
     df_selected['Tournament'] = df_selected['Tournament'].apply(normalize_tournament_name)
     df_all['Tournament'] = df_all['Tournament'].apply(normalize_tournament_name)
-
+    #st.write(df_selected.head())
     # Convert dates into years
     df_cleaned['Date'] = pd.to_datetime(df_cleaned['Date'], errors='coerce')
     df_selected['Date'] = pd.to_datetime(df_selected['Date'], errors='coerce')
@@ -76,26 +90,20 @@ def plot_winning_averages(tournament):
     df_selected = df_selected[df_selected['Year'] >= 2000]  # We only use data from 2000 onwards
     df_all = df_all[df_all['Year'] >= 2000]
     df_grouped = df_selected.groupby(['Year', 'Tournament'])['Average'].mean().reset_index()
-
+    
+    #st.write(df_grouped.head())
     # Create figure
     fig = go.Figure()
     
-    if tournament == "All" or tournament in extra_tournaments:
+    #if (major_tournaments + extra_tournaments) == (major_tournaments_all + extra_tournaments_all) or extra_tournaments != "":
         # Create line for each major tournament
-        unique_tournaments = df_grouped['Tournament'].unique()
-        for tournament in unique_tournaments:
-            df_tournament = df_grouped[df_grouped['Tournament'] == tournament]
-
-            fig.add_trace(go.Scatter(
-                x=df_tournament['Year'],
-                y=df_tournament['Average'],
-                mode='lines',
-                name=tournament
-            ))
-            
-    else:
+        #unique_tournaments = df_grouped['Tournament'].unique()
+        # Loop over each selected tournament and add a line to the plot
+    #st.write(selected_tournaments)
+    for tournament in selected_tournaments:
+        
         df_tournament = df_grouped[df_grouped['Tournament'] == tournament]
-
+        #st.write(df_tournament)
         fig.add_trace(go.Scatter(
             x=df_tournament['Year'],
             y=df_tournament['Average'],
--- app.py
+++ app.py
@@ -11,120 +11,115 @@ from Visualizations.question_8 import plot_comparison_single_team
 
 # Sidebar für Navigation
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Development of Tournaments", "Strategies and Tactics", "Players"])
+page = st.sidebar.radio("Go to", ["Info Texte", "Tournaments", "Players"])
 
-# Seite wechseln  
-if page == "Development of Tournaments":
-    subpage = st.sidebar.radio("Development of Tournaments", ["Averages throughout the years", "Price Money and Participants at WC"])
+# Seite wechseln 
+if page == "Info Texte":
+    st.title("Information Texts")
+    st.header("Header")
+    st.subheader("Subheader")
+    st.write("Test Text")
+    
+elif page == "Tournaments":
+    subpage = st.sidebar.radio("Development of Tournaments", ["Averages throughout the years", "Price Money and Participants at WC", "Location of Tournament and Winner Nationality"])
     
     # Question 2
     if subpage == "Averages throughout the years":
         tournaments = [
-            "All",
             "World Championship",
             "World Matchplay",
             "World Grand Prix",
-            "Masters of Darts",
-            "PDC US Open", 
             "Grand Slam",
             "Players Championship Finals",
             "World Cup",
-            "World Masters",
             "World Series of Darts Finals",
-            "Champions League",
             "European Tour", 
             "Players Championship", 
             "World Series"
         ]
-        selected_tournament = st.selectbox("Select a Tournament", tournaments)
         
-        st.title("2. How do the (average) winning scorings of tournaments/matches vary over the years?")
+        st.header("2. How do the averages of tournaments vary over time?")
         
-        st.title("Development of Price Money and the Number of Participants of the World Championship")
-        fig = plot_winning_averages(selected_tournament)
+        # Mehrere Turniere auswählen
+        selected_tournaments = st.multiselect("Select Tournaments", tournaments, default=["World Championship"])
+        
+        # Anzeigen des gewählten Turniers
+        st.subheader("Development of Prize Money and the Number of Participants of the World Championship")
+        
+        # Die Funktion wird mit der Liste der ausgewählten Turniere aufgerufen
+        fig = plot_winning_averages(selected_tournaments)
         st.plotly_chart(fig)
+
         
     # Question 5
     elif subpage == "Price Money and Participants at WC":
-        st.title("5. How does the price money and the amount of participants of tournaments vary over the years?")
+        st.header("5. How does the price money and number of participants vary over time?")
         
-        st.title("Participants of the World Championships over the Years")
+        st.subheader("Participants of the World Championships over the Years")
         fig = plot_participants()
         st.plotly_chart(fig)
         
-        st.title("Price Money of the World Championships over the Years")
+        st.subheader("Price Money of the World Championships over the Years")
         fig = plot_price_money()
         st.plotly_chart(fig)
         
-        st.title("Participants and Price Money of the World Championships over the Years")
+        st.subheader("Participants and Price Money of the World Championships over the Years")
         fig = plot_price_money_and_participants()
         st.plotly_chart(fig)
         
-elif page == "Strategies and Tactics":
-    subpage = st.sidebar.radio("Strategies and Tactics", ["Location of Tournament and Winner Nationality", "Team vs. Single", "Double Fields"])
-    
-    # Question 7
+        # Question 7
     if subpage == "Location of Tournament and Winner Nationality":
-        st.title("7. In which countries are tournaments held and how does it influence singular participants if at all?")
+        st.header("7. How does the country a tournament is held in correlate to the success of players?")
         
-        st.title("Observed Frequencies")
+        st.subheader("Observed Frequencies")
         fig = plot_observed_frequencies()
         st.plotly_chart(fig)
         
-        st.title("Difference between Observed and Expected Frequencies")
+        st.subheader("Difference between Observed and Expected Frequencies")
         fig = plot_observed_expected_frequencies()
         st.plotly_chart(fig)
         
-        st.title("Conditional Probabilities")
+        st.subheader("Conditional Probabilities")
         fig = plot_conditional_probability()
         st.plotly_chart(fig)
+    
+elif page == "Players":
+    subpage = st.sidebar.radio("Players", ["Averages of Best Players", "Rankings vs Properties", "Team vs. Single", "Double Fields"])
         
     # Question 8
-    elif subpage == "Team vs. Single":
-        st.title("8. How do team matches compare to single matches in their scores? (does it imply teammates dragging each other down or raising each other up?)")
+    if subpage == "Team vs. Single":
+        st.header("8. Wie sehr weicht die Teamperformance von der Einzelperformance ab?")
         
-        st.title("Comparision of Averages of the Team Matches to the Averages of the Single Player")
+        st.subheader("Comparision of Averages of the Team Matches to the Averages of the Single Player")
         fig = plot_comparison_single_team()
         st.plotly_chart(fig)
     
-    # Question 4   
+    # Question 15   
     elif subpage == "Double Fields":
-        st.title("4. What are the most popular double fields and what is the percentage to check them? (doubles are normally not thrown)")
-        
-        st.title("Double Fields Analysis")
-
-        # Visualisierungen mit gewählten Parametern aufrufen
-        #st.title("Hit and Misses of individual Double Fields")
-        #fig = plot_doubles_fields_hits_misses()
-        #st.plotly_chart(fig)
-
-        st.title("Distribution of Throws and Hits on Double Fields")
-        fig = plot_distribution_double_fields()
-        st.plotly_chart(fig)        
+        st.header("15. How does the performance of specific/ individual players change over time?")      
         
         # Lade eine Liste von Spielern aus einer CSV oder definiere sie manuell
-        players = ["Luke Humphries", "Luke Littler", "Michael van Gerwen"]  # Ersetze dies mit echten Namen oder lade aus einer Datei
+        players = ["Luke Humphries", "Luke Littler", "Michael van Gerwen", "Phil Taylor", "Stephen Bunting",
+                   "Rob Cross", "Gerwyn Price", "Nathan Aspinall", "Chris Dobey", "Gary Anderson", "James Wade",
+                   "Peter Wright", "Martin Schindler"]  # Ersetze dies mit echten Namen oder lade aus einer Datei
     
         # Dropdowns für Auswahl des Spielers und Doppelfelds
         selected_player = st.selectbox("Select a Player", players)
 
-        st.title(f"Doubles Field Values of {selected_player}")
+        st.subheader(f"Doubles Field Values of {selected_player}")
         fig = plot_double_fields_player(selected_player)
         st.plotly_chart(fig)
         
         double_fields = [f"D{i}" for i in range(1, 21)] + ["D25"]  # Doppelfelder 1-20, Bullseye D25
         selected_double = st.selectbox("Select a Double Field", double_fields)
 
-        st.title(f"Throws on {selected_double} and its Double Quota for {selected_player}")
+        st.subheader(f"Throws on {selected_double} and its Double Quota for {selected_player}")
         fig = plot_double_fields_player_combined(selected_player, selected_double)
-        st.plotly_chart(fig)
-        
-elif page == "Players":
-    subpage = st.sidebar.radio("Players", ["Averages of Best Players", "Rankings vs Properties"])
+        st.plotly_chart(fig) 
     
-    # Question 1
-    if subpage == "Averages of Best Players":
-        st.title("1. How do the 5/10/15 best player change from 2000/1995 to now? (averages vs. check-out quota)")
+    # Question 14
+    elif subpage == "Averages of Best Players":
+        st.header("14. How does the performance of players in general change over time?")
         
         category = ["Averages", "Checkout Quota"] 
         ranking_positions = [5, 10, 15, 20] 
@@ -132,29 +127,40 @@ elif page == "Players":
         selected_ranking_position = st.selectbox("Select a Number of Ranking Positions", ranking_positions)
         
         if selected_category == "Averages":
-            st.title("Development of Averages in 2009 vs. 2024")
+            st.subheader("Development of Averages in 2009 vs. 2024")
             fig = plot_average_2009_2024(selected_ranking_position)
             st.plotly_chart(fig)
             
-            st.title("Development of Averages over the Years")
+            st.subheader("Development of Averages over the Years")
             fig = plot_average_every_year()
             st.plotly_chart(fig)
             
         elif selected_category == "Checkout Quota":
-            st.title("Development of Checkouts in 2012 vs. 2024")
+            st.subheader("Development of Checkouts in 2012 vs. 2024")
             fig = plot_checkout_2012_2024(selected_ranking_position)
             st.plotly_chart(fig)
             
-            st.title("Development of Checkouts over the Years")
+            st.subheader("Development of Checkouts over the Years")
             fig = plot_checkout_every_year()
             st.plotly_chart(fig)
+            
+        st.header("Double Fields Analysis")
+
+        # Visualisierungen mit gewählten Parametern aufrufen
+        #st.title("Hit and Misses of individual Double Fields")
+        #fig = plot_doubles_fields_hits_misses()
+        #st.plotly_chart(fig)
+
+        st.subheader("Distribution of Throws and Hits on Double Fields")
+        fig = plot_distribution_double_fields()
+        st.plotly_chart(fig)  
         
     # Question 6
     elif subpage == "Rankings vs Properties":
-        st.title("6. How do the rankings vary over age, nationality and left/right handed people of participants?")
+        st.header("6. How does age, nationality and handiness eGects the rankings?")
         
         var = 10
         
-        st.title("Participants Over the Years")
+        st.subheader("Participants Over the Years")
         fig = plot_ranking_age(var)
         st.plotly_chart(fig)
