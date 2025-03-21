381bbf83dfc7a9a9afa2a9210a41cedeba39e620
Tyra <tyra@example.com>
2025-03-21 08:59:18 +0100

    design homepage text in app.py

--- app.py
+++ app.py
@@ -79,32 +79,27 @@ page = st.sidebar.radio("Go to", ["Home", "Tournament Data", "Player Data", "Dat
 if page == "Home":
     # Set page title and icon
     st.title("Welcome to the Darts Data Science Project! 🎯")
-    st.header("Exploring Insights Through Data")
+    st.header("Dive into the analysis and uncover the hidden patterns in the world of darts!")
 
     # Introduction
     st.subheader(introduction)
-
     with st.expander("Game Explanation"):
         st.write(darts_explanation)
 
     # Research Questions
     st.subheader("Research Questions Explored")
-    st.write("- **Subtopic: Tournament Data**")
-    st.write("- **How do the averages of tournaments vary over time?**")
-    st.write("- **How does the price money and number of participants vary over time?**")
-    st.write("- **How does the country a tournament is held in correlate to the success of players?**")
-    st.write("- **Subtopic: Player Data**")
-    st.write("- **How does the performance of players in general change over time?**")
-    st.write("- **How does the performance of individual players change over time?**")
-    st.write("- **How does age, nationality and handiness effects the rankings?**")
-    st.write("- **Is there a difference between a player's team performance and single performance?**")
-    st.write("- **How likely is it to throw 180 points after there was another 180 points thrown?**")
-    st.write("- **How likely are participants to win a leg after throwing 180 points as first throw?**")
+    st.write("**Subtopic: Tournament Data**")
+    st.write("- How do the averages of tournaments vary over time?")
+    st.write("- How does the price money and number of participants vary over time?")
+    st.write("- How does the country a tournament is held in correlate to the success of players?")
+    st.write("**Subtopic: Player Data**")
+    st.write("- How does the performance of players in general change over time?")
+    st.write("- How does the performance of individual players change over time?")
+    st.write("- How does age, nationality and handiness effects the rankings?")
+    st.write("- Is there a difference between a player's team performance and single performance?")
+    st.write("- How likely is it to throw 180 points after there was another 180 points thrown?")
+    st.write("- How likely are participants to win a leg after throwing 180 points as first throw?")
 
-    # Call to Action
-    st.subheader(
-        "Dive into the analysis and uncover the hidden patterns in the world of darts!"
-    )
     # Call function to add footer
     add_footer()
 
@@ -113,7 +108,7 @@ elif page == "Tournament Data":
         "Tournament Data",
         [
             "2 Averages over Time",
-            "5 Price Money and Participants at WC",
+            "5 Price Money and Participants at the World Cup",
             "7 Locations of Tournaments and Winner Nationality"
         ]
     )
@@ -158,7 +153,7 @@ elif page == "Tournament Data":
         add_footer()
         
     # Question 5
-    elif subpage == "5 Price Money and Participants at WC":
+    elif subpage == "5 Price Money and Participants at the World Cup":
         st.header("5. How does the price money and number of participants vary "
                  "over time?")
         
