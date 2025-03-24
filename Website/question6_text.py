456d84b03ce7f64d10d64e61e3cdfb76da413870
Tyra <tyra@example.com>
2025-03-24 15:06:16 +0100

    question 6 texts

--- Website/question6_text.py
+++ Website/question6_text.py
@@ -1,10 +1,12 @@
 explanation_6 = """ """
 
-first_description_6 = """ """
+first_description_6 = """This graph shows the player's age demographics related to their rankings. The bigger the bubble, the more players belong to 
+that demographic. (Sara, pls help with the legend)"""
 
 first_graph_6 = """The peak performance age for players seems to be 30-50 years, with some older players (50-60) still ranking well."""
 
-second_description_6 = """ """
+second_description_6 = """This graph shows the player's nationality demographics related to their rankings. The bigger the bubble, the more players
+belong to that demographic. (Sara, pls help with the legend)"""
 
 second_graph_6 = """England appears most frequently, indicating that English players may dominate the rankings.
 
@@ -12,7 +14,8 @@ The majority of top-ranking players come from England, suggesting that the sport
 Other countries like Germany, Japan, and Australia have representation, but their players are less frequent.
 Players from these countries might just be less common."""
 
-third_description_6 = """ """
+third_description_6 = """This graph shows the player's right-/left-handedness demographics related to their rankings. The bigger the bubble, the more players
+belong to that demographic. (Sara, pls help with the legend)"""
 
 third_graph_6 = """The majority of high-ranking players are right-handed, as shown by the larger bubbles on the left.
 Left-handed players are significantly fewer and rarely appear in top ranks.
