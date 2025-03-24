9c06082a080456851cda11acdbe284836f3d96f6
EmilieTr <emilie@terhaar.info>
2025-03-24 10:57:16 +0100

    format page question 7

--- Website/question7_web.py
+++ Website/question7_web.py
@@ -8,32 +8,28 @@ from Visualizations.question_7 import (
 )
     
 def question7_web():
-    st.header(
-        "7. How does the country a tournament is held in correlate "
-        "to the success of players?"
-    )
-
-    with st.expander("Interpretation"):
+    st.title("Host Country vs Nationality")
+    st.markdown("---")
+    st.subheader(
+        "How does the country a tournament is held in correlate to the success of players?")
+    with st.expander("Explanation"):
         st.write(heads_up_7)
     
-    st.subheader("Observed Frequencies")
+    
     fig = plot_observed_frequencies()
     st.plotly_chart(fig)
-
-    with st.expander("Interpretation"):
+    st.text("There is a significant relationship between the host country and nationality.")
+    with st.expander("Interpretation and critical evaluation"):
         st.write(first_graph_7)
     
-    st.subheader("Difference between Observed and Expected Frequencies")
+
     fig = plot_observed_expected_frequencies()
     st.plotly_chart(fig)
-
-    with st.expander("Interpretation"):
+    st.text("There is no significant relationship between the host country and nationality.")
+    with st.expander("Interpretation and critical evaluation"):
         st.write(second_graph_7)
         
     st.image("Visualizations/question_7/map.png")
-
-    with st.expander("Interpretation"):
-        st.write(fourth_graph_7)
 
     # Call function to add footer
     add_footer()
