2f3dc5cda33c52ebe09db6d511972874a0a1e751
EmilieTr <emilie@terhaar.info>
2025-03-24 11:54:29 +0100

    edit format question 1, 6, 15

--- Website/question1_web.py
+++ Website/question1_web.py
@@ -9,13 +9,11 @@ from Visualizations.question_1 import (
 )
 
 def question1_web():    
-    st.header("1. How does the general performance of players change over "
-                "time?")
-    
+    st.title("Averages of the best")
+    st.markdown("---")
     category = ["Averages", "Checkout Quota"]
     selected_ranking_position = st.slider("Choose the number of ranking positions", min_value=2, max_value=33)
     selected_category = st.selectbox("Select a Category", category)
-    
     if selected_category == "Averages":
         years = [str(i) for i in range(2009, 2025)]
 
@@ -25,21 +23,6 @@ def question1_web():
             years,
             default=["2009", "2024"]
         )
-    
-        st.subheader("Development of Averages in 2009 vs. 2024")
-        fig = plot_average_line_chart(selected_ranking_position, selected_years)
-        st.plotly_chart(fig)
-
-        with st.expander("Interpretation"):
-            st.write(first_graph_1)
-        
-        st.subheader("Development of Averages over the Years")
-        fig = plot_average_every_year(selected_ranking_position)
-        st.plotly_chart(fig)
-
-        with st.expander("Interpretation"):
-            st.write(second_graph_1)
-        
     elif selected_category == "Checkout Quota":
         years = [str(i) for i in range(2012, 2025)]
 
@@ -49,20 +32,47 @@ def question1_web():
             years,
             default=["2012", "2024"]
         )
+    
+    st.markdown("---")
+    st.subheader("How does the general performance of players change over "
+                "time?")
+    
+    with st.expander("Explanation"):
+        st.text("Explanation")
+
+    if selected_category == "Averages":
+
+        fig = plot_average_line_chart(selected_ranking_position, selected_years)
+        st.plotly_chart(fig)
+
+        st.text("Hier kommt eine kurze Beschreibung der Ergebnisse hin.")   
+        with st.expander("Interpretation and critical evaluation"):
+            st.write(first_graph_1)
+
+        fig = plot_average_every_year(selected_ranking_position)
+        st.plotly_chart(fig)
+
+        st.text("Hier kommt eine kurze Beschreibung der Ergebnisse hin.")
+        with st.expander("Interpretation and critical evaluation"):
+            st.write(second_graph_1)
         
-        st.subheader("Development of Checkouts in 2012 vs. 2024")
+    elif selected_category == "Checkout Quota":
+        
         fig = plot_checkout_line_chart(selected_ranking_position, selected_years)
         st.plotly_chart(fig)
         
-        st.subheader("Development of Checkouts over the Years")
+        st.text("Hier kommt eine kurze Beschreibung der Ergebnisse hin.")
+        with st.expander("Interpretation and critical evaluatiion"):
+            st.text("Interpretation and critical evaluation")
+
         fig = plot_checkout_every_year(selected_ranking_position)
         st.plotly_chart(fig)
-
-        with st.expander("Interpretation"):
+        
+        st.text("Hier kommt eine kurze Beschreibung der Ergebnisse hin.")
+        with st.expander("Interpretation and critical evaluation"):
             st.write(third_graph_1)
-    
-        with st.expander("General Interpretation"):
-            st.write(interpretation_1)   
+            st.write(interpretation_1) 
+              
     
     # Call function to add footer
     add_footer()
