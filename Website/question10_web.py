21c5566f0130c63da660ec2a1757d5601954bc63
EmilieTr <emilie@terhaar.info>
2025-03-24 11:19:10 +0100

    edit format question 10

--- Website/question10_web.py
+++ Website/question10_web.py
@@ -6,19 +6,26 @@ from Visualizations.question_10 import plot_histogram
 from Visualizations.question_10 import t_test
 
 def question10_web():
-    st.header("10. How likely are participants win a leg after throwing a 180 as first throw?") 
-    
+    st.title("Winning legs vs. 180 openings")
+    st.markdown("---")
+
+    st.subheader("How likely are participants win a leg after throwing a 180 as first throw?") 
+    with st.expander("Explanation"):
+        st.text("Explanation")
     df = pd.read_csv('Visualizations/question_10/180_stats.csv') 
     st.dataframe(df)
-    
-    st.subheader("Histogram")
+    st.text("Hier kommt eine kurze Beschreibung vom Diagramm hin.")
+    with st.expander("Interpretation and critical evaluation"):
+        st.text("Interpretation and critical evaluation")
+
     fig = plot_histogram("Probability (%)")
     st.plotly_chart(fig)
-
-    with st.expander("Interpretation"):
+    st.text("Hier kommt eine kurze Beschreibung vom Diagramm hin.")
+    with st.expander("Interpretation and critical evaluation"):
+        st.write("T-Test between European Tour and Majors")
+        st.write(t_test())
         st.write(first_graph_10)
         
-    st.write("T-Test between European Tour and Majors")
-    st.write(t_test())
+    
         
     add_footer()
