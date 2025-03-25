55c900fd64b258205e6a83d927448c717d8cb467
Tyra <tyra@example.com>
2025-03-25 14:25:00 +0100

    home edit

--- Website/texts.py
+++ Website/texts.py
@@ -1,17 +1,17 @@
-question_2 = "How do the averages of tournaments vary over time?"
+question_2 = "- How do the averages of tournaments vary over time?"
 
-question_5 = "How does the price money and number of participants of the World Championship vary over time?"
+question_5 = "- How does the price money and number of participants of the World Championship vary over time?"
 
-question_7 = "How does the country a tournament is held in correlate to the success of players?"
+question_7 = "- How does the country a tournament is held in correlate to the success of players?"
 
-question_4 = "What are most popular double fields and their corresponding checkout percentages?"
+question_4 = "- What are most popular double fields and their corresponding checkout percentages?"
 
-question_9 = "How likely is it to throw a 180 after the opponent threw one?"
+question_9 = "- How likely is it to throw a 180 after the opponent threw one?"
 
-question_10 = "How likely are participants to win a leg after throwing a 180 as first throw?"
+question_10 = "- How likely are participants to win a leg after throwing a 180 as first throw?"
 
-question_1 = "How does the general performance of players change over time?"
+question_1 = "- How does the general performance of players change over time?"
 
-question_6 = "How does age, nationality and left-/right-handedness effect the rankings?"
+question_6 = "- How does age, nationality and left-/right-handedness effect the rankings?"
 
 question_15 = "- How does the performance of individual players change over time?"
