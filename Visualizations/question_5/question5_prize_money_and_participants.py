84b0f4d16efd8307b2b5ad4b07cf13fdffb80a01
EmilieTr <emilie@terhaar.info>
2025-03-25 14:54:14 +0100

    fixed error with empty selection for question 5

--- Visualizations/question_5/question5_prize_money_and_participants.py
+++ Visualizations/question_5/question5_prize_money_and_participants.py
@@ -4,6 +4,39 @@ import plotly.express as px
 
 
 def plot_prize_money_and_participants(selected):
+    if not selected:
+        # Create empty chart, if selected is empty
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(
+        x=[],  
+        y=[],  
+        name="Prize Money",
+        yaxis="y"
+        ))
+        fig.add_trace(go.Scatter(
+        x=[],  
+        y=[],  
+        name="Participants",
+        yaxis="y2"
+        ))
+        #  Adjust layout
+        fig.update_layout(
+            title="Development of Prize Money and Participants over the Years (PDC World Championship)",
+            xaxis=dict(
+                title="Year"
+            ),
+            yaxis=dict(
+                title="Prize Money (£)",
+                side="left"
+            ),
+            yaxis2=dict(
+                title="Participants",
+                overlaying="y",
+                side="right"
+            ),
+            margin=dict(l=50, r=50, t=50, b=50)
+        )
+        return fig
     """
     Create a plot showing prize money and participants over years."
     """
