aeacedec4d99067ec5c10940623e479423e8288d
EmilieTr <emilie@terhaar.info>
2025-03-31 15:55:34 +0200

    change names and especially question 15

--- Visualizations/question_15/__init__.py
+++ Visualizations/question_15/__init__.py
@@ -1,3 +1,3 @@
-from .question4_double_fields_player import plot_double_fields_player
-from .question4_double_fields_player_combined import plot_double_fields_player_combined
-from .question4_averages import plot_player_average
+from .question15_double_fields_player import plot_double_fields_player
+from .question15_double_fields_player_combined import plot_double_fields_player_combined
+from .question15_averages import plot_player_average
